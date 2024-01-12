cccefad293264ebcb2e631a681c5ed79aed8dca9
janani <jananiselva2618@gmai.com>
2024-01-12 16:43:06 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Bootstrap from './bootstrap';
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
+  const array = [{ value: "free", price: "0", users: "single user", subdomain: "free subdomain" },
+  { value: "plus", price: "9", users: "5user", subdomain: "free subdomain" },
+  { value: "pro", price: "49", users: "unlimited user", subdomain: "unlimited free subdomain" }];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section class="pricing py-5">
+      <div class="container">
+        <div class="row">
+          {array.map((content) => {
+            return (
+           <Bootstrap main={content}></Bootstrap>
+            )
+          })}
+        </div>
+      </div>
+    </section>
   );
 }
 
